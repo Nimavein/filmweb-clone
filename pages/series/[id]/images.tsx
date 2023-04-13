ad12dfcbdf88e0f43a5e525acde9f133eee0a0c3
AzureAD\MichalZygiel <michal.zygiel@noaignite.com>
2023-04-13 15:19:07 +0200

    fix build

--- pages/series/[id]/images.tsx
+++ pages/series/[id]/images.tsx
@@ -9,7 +9,7 @@ const SeriesImages = () => {
   const { id } = router.query;
 
   const dispatch = useAppDispatch();
-  const { images } = useAppSelector((state) => state.series);
+  const images = useAppSelector((state) => state.series.details?.images);
 
   useEffect(() => {
     if (id && !images) dispatch(fetchSeriesData(Number(id)));
