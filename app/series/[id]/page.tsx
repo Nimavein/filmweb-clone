193aba107a53dbde1ce4acbcd096510833827e8c
AzureAD\MichalZygiel <michal.zygiel@noaignite.com>
2023-05-09 22:22:59 +0200

    test

--- app/series/[id]/page.tsx
+++ app/series/[id]/page.tsx
@@ -1,14 +1,5 @@
 "use client";
-
-import { useAppDispatch, useAppSelector } from "@/store";
-import { fetchSeriesData } from "@/store/seriesSlice";
-import { useEffect } from "react";
-import SeriesContent from "./components/Series";
-import { PageIdParams } from "@/types/types";
-
-const Series = ({ params: { id } }: PageIdParams) => {
-  const dispatch = useAppDispatch();
-  const { details } = useAppSelector((state) => state.series);
+const Series = () => {
 
   return <></>;
 };
